d80150d278420eab3daee5746e7afebdb62fbb1a
Sen-Zhang <senzhang1987@126.com>
2015-12-14 19:35:59 -0800

    refactor installers

--- lib/generators/simple_switch/install_generator.rb
+++ lib/generators/simple_switch/install_generator.rb
@@ -6,10 +6,20 @@ module SimpleSwitch
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../../templates', __FILE__)
 
-      desc 'Copy initializer file simple_switch.rb to config/initializers.'
+      desc 'Copy required files for each strategy'
 
-      def copy_initializer
-        copy_file 'simple_switch.rb', 'config/initializers/simple_switch.rb'
+      def copy_files
+        case SimpleSwitch.feature_store
+        when :database
+          # Copy the migration file to db/migrate.
+          copy_file 'migration.rb',
+                    "db/migrate/#{Time.now.utc.strftime('%Y%m%d%H%M%S')}_create_simple_switch_tables.simple_switch.rb"
+        when :yml
+          # Copy feature configuration sample file feature_config_sample.yml to customized location.
+          copy_file 'feature_config_sample.yml',
+                    "#{SimpleSwitch.feature_config_file_dir}/#{SimpleSwitch.feature_config_file_name}"
+        else
+        end
       end
     end
 
